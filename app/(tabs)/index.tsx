2a2449522fe5a2cec37f6bcd3f2070878ac0e599
Wissal65 <wissalbara65@gmail.com>
2024-06-03 13:01:40 +0100

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
+import React, { useEffect,useState } from 'react'
 import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  useEffect(() => {
+      const hideTabBar = () => navigation.setOptions({ tabBarStyle: { display: 'none' } });
+      const showTabBar = () => navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+  
+      // Hide tab bar when entering the screen
+      hideTabBar();
+  
+      // Show tab bar when leaving the screen
+      return () => showTabBar();
+    }, [navigation]);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
